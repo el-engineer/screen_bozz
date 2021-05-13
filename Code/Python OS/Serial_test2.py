aacca85c0451f3ba923678263ee5407bf2bcd1bd
Jose Luis Hotema <81241879+el-engineer@users.noreply.github.com>
2021-05-13 16:20:13 -0500

    Create Serial_test2.py
    test to see if you can send a single line of g-code to arduino/Shield (marlin)

---
+++ Code/Python OS/Serial_test2.py
@@ -0,0 +1,26 @@
+import serial
+import time
+
+#Connect to the Arduino Mega 2560 over serial connection
+if __name__ == '__main__':
+  ser = serial.Serial("/dev/ttyACM0",250000, timeout=1) 
+  ser.flush()
+
+ListofPositiveResponses = ["Y", "y", "Yes", "yes"]
+ListofNegativeResponses = ["N", "n", "No", "no"]
+
+print("Perform an auto home before starting? (Y/N) ")
+UserManualGCodeInput = input("")
+if UserManualGCodeInput in ListofPositiveResponses:
+    HomingCommandInBytes = bytes(("G28" + " \n"), 'UTF-8')
+    ser.write(HomingCommandInBytes)
+    print("Homing. This will take approximately 2 minutes. ")
+    time.sleep(120)
+
+print("Move to the default starting position of X150.0 Y90.0 Z70.0? (Y/N)  ")
+UserManualGCodeInput = input("")
+if UserManualGCodeInput in ListofPositiveResponses:
+    MoveToSpindleStartingPositionInBytes = bytes(("G1X140Y95Z10" + " \n"), 'UTF-8')
+    ser.write(MoveToSpindleStartingPositionInBytes)
+    print("Moving to spindle starting position. This will take approximately 1 minutes. ")
+    time.sleep(60)
